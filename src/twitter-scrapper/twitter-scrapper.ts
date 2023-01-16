c8c17ce442d99278f883ef80b7a4e2ff2b14e279
Artandrey <artandrey7777@gmail.com>
2023-01-16 14:03:52 +0200

    added browser closing

--- src/twitter-scrapper/twitter-scrapper.ts
+++ src/twitter-scrapper/twitter-scrapper.ts
@@ -6,124 +6,125 @@ import scrapPage, { PageData } from './scrap-page';
 import '../config';
 
 const BROWSER_CONFIG = {
-    userDataDir: './browser-data',
-    headless: !!+process.env.HEADLESS!,
-    executablePath: process.env.CHROME_PATH,
-    args: ['--disable-notifications', '--no-sandbox'],
+  userDataDir: './browser-data',
+  headless: !!+process.env.HEADLESS!,
+  executablePath: process.env.CHROME_PATH,
+  args: ['--disable-notifications', '--no-sandbox'],
 };
 
 const checkIsUsernameRequired = (page: Page): Promise<boolean> => {
-    return new Promise((resolve) => {
-        page.waitForSelector('[data-testid="ocfEnterTextTextInput"]').then(() =>
-            resolve(true)
-        );
-        page.waitForSelector('[type="password"]').then(() => resolve(false));
-    });
+  return new Promise((resolve) => {
+    page
+      .waitForSelector('[data-testid="ocfEnterTextTextInput"]')
+      .then(() => resolve(true));
+    page.waitForSelector('[type="password"]').then(() => resolve(false));
+  });
 };
 
 const checkIsCodeRequired = (page: Page): Promise<boolean> => {
-    return new Promise((resolve) => {
-        page.waitForSelector('[data-testid="ocfEnterTextTextInput"]').then(() =>
-            resolve(true)
-        );
-        page.waitForNavigation().then(() => resolve(false));
-    });
+  return new Promise((resolve) => {
+    page
+      .waitForSelector('[data-testid="ocfEnterTextTextInput"]')
+      .then(() => resolve(true));
+    page.waitForNavigation().then(() => resolve(false));
+  });
 };
 
 export const authorize = async () => {
-    const browser = await puppeteer.launch(BROWSER_CONFIG);
-    const page = await browser.newPage();
-    await page.setViewport({
-        width: 1366,
-        height: 768,
-    });
-    const url = 'https://twitter.com/login';
-    page.goto(url, {
-        timeout: 0,
-        waitUntil: 'networkidle0',
-    });
-    await page.waitForNavigation();
-    await page.waitForSelector('[autocomplete="username"]');
-    await page.type('[autocomplete="username"]', process.env.EMAIL!, {
-        delay: 100,
-    });
+  const browser = await puppeteer.launch(BROWSER_CONFIG);
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1366,
+    height: 768,
+  });
+  const url = 'https://twitter.com/login';
+  page.goto(url, {
+    timeout: 0,
+    waitUntil: 'networkidle0',
+  });
+  await page.waitForNavigation();
+  await page.waitForSelector('[autocomplete="username"]');
+  await page.type('[autocomplete="username"]', process.env.EMAIL!, {
+    delay: 100,
+  });
 
+  await page.keyboard.press('Enter');
+  const usernameIsRequired = await checkIsUsernameRequired(page);
+  if (usernameIsRequired) {
+    await page.type(
+      '[data-testid="ocfEnterTextTextInput"]',
+      process.env.TWITTER_USERNAME!,
+      { delay: 100 }
+    );
     await page.keyboard.press('Enter');
-    const usernameIsRequired = await checkIsUsernameRequired(page);
-    if (usernameIsRequired) {
-        await page.type(
-            '[data-testid="ocfEnterTextTextInput"]',
-            process.env.TWITTER_USERNAME!,
-            { delay: 100 }
-        );
-        await page.keyboard.press('Enter');
-    }
-    await page.waitForSelector('[type="password"]');
-    await page.type('[type="password"]', process.env.PASSWORD!, { delay: 100 });
+  }
+  await page.waitForSelector('[type="password"]');
+  await page.type('[type="password"]', process.env.PASSWORD!, { delay: 100 });
+  await page.keyboard.press('Enter');
+  const codeIsRequired = await checkIsCodeRequired(page);
+  if (codeIsRequired) {
+    const code = await prompt('Code is required, enter it');
+    await page.type('[data-testid="ocfEnterTextTextInput"]', code);
     await page.keyboard.press('Enter');
-    const codeIsRequired = await checkIsCodeRequired(page);
-    if (codeIsRequired) {
-        const code = await prompt('Code is required, enter it');
-        await page.type('[data-testid="ocfEnterTextTextInput"]', code);
-        await page.keyboard.press('Enter');
-    }
-    console.log('AUTHORISED!');
-    await browser.close();
+  }
+  console.log('AUTHORISED!');
+  await browser.close();
 };
 
 const checkIsAuthorised = async (page: Page) => {
-    try {
-        await page.goto('https://twitter.com/');
-        await page.waitForNavigation();
+  try {
+    await page.goto('https://twitter.com/');
+    await page.waitForNavigation();
 
-        const url = page.url();
+    const url = page.url();
 
-        return url.includes('home');
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
+    return url.includes('home');
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 };
 
 const scrapImagesWithLinksByUsername = async (
-    username: string,
-    ignorePostIDs: string[] = []
+  username: string,
+  ignorePostIDs: string[] = []
 ) => {
-    const browser = await puppeteer.launch(BROWSER_CONFIG);
-    const page = await browser.newPage();
-    page.setDefaultTimeout(0);
-    page.setDefaultNavigationTimeout(0);
-    await page.setViewport({
-        width: 1366,
-        height: 768,
-    });
-    // const authResult = await checkIsAuthorised(page);
-    // console.log(authResult);
+  const browser = await puppeteer.launch(BROWSER_CONFIG);
+  const page = await browser.newPage();
+  page.setDefaultTimeout(0);
+  page.setDefaultNavigationTimeout(0);
+  await page.setViewport({
+    width: 1366,
+    height: 768,
+  });
+  // const authResult = await checkIsAuthorised(page);
+  // console.log(authResult);
 
-    // if (!authResult) {
-    //     throw Error('Not authorised');
-    // }
-    await page.goto('https://twitter.com/' + username, {
-        timeout: 0,
-        waitUntil: 'networkidle2',
-    });
+  // if (!authResult) {
+  //     throw Error('Not authorised');
+  // }
+  await page.goto('https://twitter.com/' + username, {
+    timeout: 0,
+    waitUntil: 'networkidle2',
+  });
 
-    const links = (await collectAllLinks(page)).filter(
-        (link) => !ignorePostIDs.includes(link)
-    );
+  const links = (await collectAllLinks(page)).filter(
+    (link) => !ignorePostIDs.includes(link)
+  );
 
-    const pagesWorker = new PagesWorker<string, PageData>(
-        browser,
-        {
-            width: 1366,
-            height: 768,
-        },
-        scrapPage
-    );
+  const pagesWorker = new PagesWorker<string, PageData>(
+    browser,
+    {
+      width: 1366,
+      height: 768,
+    },
+    scrapPage
+  );
 
-    await pagesWorker.createPages(1);
-    pagesWorker.registerTasks(...links);
-    const result = await pagesWorker.run();
-    return result;
+  await pagesWorker.createPages(1);
+  pagesWorker.registerTasks(...links);
+  const result = await pagesWorker.run();
+  await browser.close();
+  return result;
 };
 export default scrapImagesWithLinksByUsername;
